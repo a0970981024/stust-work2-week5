5f90eb72149839099b6da456ad36fe91a564754f
日平 趙 <4b1g0034@office.stust.edu.tw>
2023-03-19 21:13:19 +0800

    判斷是否為迴文

--- vs-work2-week5.cpp
+++ vs-work2-week5.cpp
@@ -1,20 +1,33 @@
-﻿// vs-work2-week5.cpp : 此檔案包含 'main' 函式。程式會於該處開始執行及結束執行。
-//
-
+﻿#include <iomanip>
 #include <iostream>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+int main() {
+	int x = -1, total = 0;
+	char temp[9999] = { 0 }, cptemp[9999] = { 0 };
+	cout << "請輸入一整數:";
+	cin >> temp;
+	
+	for (int i = 0; i < 9998; i++) {
+		if (temp[i] != 0) {
+			cptemp[i] = temp[i];
+		//	cout << "temp[" << i << "]=" << temp[i]<<endl;
+		//	cout << "cptemp[" << i << "]=" << cptemp[i] << endl;
+			x++;
+		}
+	}
+
+	int cpx = x;
+	for (int i = 0; i < x; i++) {
+		if (cptemp[cpx] == temp[i]) {
+			total++;
+			cpx--;
+		}
+	}
+
+	if (total == x) {
+		cout << "是" << endl;
+	}else {
+		cout << "否" << endl;
+	}
 }
-
-// 執行程式: Ctrl + F5 或 [偵錯] > [啟動但不偵錯] 功能表
-// 偵錯程式: F5 或 [偵錯] > [啟動偵錯] 功能表
-
-// 開始使用的提示: 
-//   1. 使用 [方案總管] 視窗，新增/管理檔案
-//   2. 使用 [Team Explorer] 視窗，連線到原始檔控制
-//   3. 使用 [輸出] 視窗，參閱組建輸出與其他訊息
-//   4. 使用 [錯誤清單] 視窗，檢視錯誤
-//   5. 前往 [專案] > [新增項目]，建立新的程式碼檔案，或是前往 [專案] > [新增現有項目]，將現有程式碼檔案新增至專案
-//   6. 之後要再次開啟此專案時，請前往 [檔案] > [開啟] > [專案]，然後選取 .sln 檔案
