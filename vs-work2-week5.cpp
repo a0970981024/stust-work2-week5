ded735c3f10bdd7f69ca390ca3dc9d6c3e6e672c
日平 趙 <4b1g0034@office.stust.edu.tw>
2023-03-20 18:11:40 +0800

    撲克牌比大小

--- vs-work2-week5.cpp
+++ vs-work2-week5.cpp
@@ -1,5 +1,6 @@
 ﻿#include <iomanip>
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main() {
@@ -9,12 +10,17 @@ int main() {
 	cout << "請輸入有幾疊牌:";
 	cin >> x;
 	string tile[52][52] ;
+	getline(cin, temp);
 	
 	for (int i = 1 ; i <= x; i++) {
 		
 		cout << "請輸入第" << i << "疊牌:";
 		for (int j = 0; j < 52; j++) {
-
+			if (c = getchar() == 'x') {
+				return 0;
+			}
+			cin >> tile[i][j];
+			
 		}
 	}
 
