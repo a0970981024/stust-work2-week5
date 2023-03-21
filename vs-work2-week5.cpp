3d7257dd22d338077669d8b3670812835f34d0ab
日平 趙 <4b1g0034@office.stust.edu.tw>
2023-03-21 23:16:38 +0800

    2023-03-21 不知道怎麼把英文做大小的比較

--- vs-work2-week5.cpp
+++ vs-work2-week5.cpp
@@ -4,29 +4,43 @@
 using namespace std;
 
 int main() {
-	int x;
-	char c;
-	string temp;
-	cout << "請輸入有幾疊牌:";
-	cin >> x;
-	string tile[52][52] ;
-	getline(cin, temp);
+	int x,y=1;
+	char temp[52] = {0},ctile[52][52],yy[52]={0};
 	
-	for (int i = 1 ; i <= x; i++) {
-		
-		cout << "請輸入第" << i << "疊牌:";
-		for (int j = 0; j < 52; j++) {
-			if (c = getchar() == 'x') {
-				return 0;
+	char y[] = {'a','1','b','1','c','1'};
+	string stile[52][52] ;
+	cout << "請輸入有幾疊牌:";
+	cin >> x;//x=2
+	
+	for (int i = 0 ; i < x; i++) {	//跑兩圈
+		cout << "請輸入第" << i+1 << "疊牌:";
+		for (int j = 0; j < 52; j++) {  
+			cin >> stile[i][j];
+			if (cin.get() == '\n') {
+				j = 52;
+			}else{
+				y++;
 			}
-			cin >> tile[i][j];
+		}
+		temp[x] = y;
+		y = 0;
+	}
+
+	//if(stile[1][0]=='')
+	//cout << "Y=" << y << endl;
+	for (int i = 0; i < x; i++) {
+		for (int j = 0; j < temp[x]; j++) {
+			
 			
 		}
 	}
 
-	for(int i = 1; i <= x; i++) {
-		for (int j = 0; j < 52; j++) {
-			cout << tile[i][j] << endl;
+
+
+	for(int i = 0; i < x; i++) {
+		for (int j = 0; j < 52 ; j++) {
+			//cout << yy[j] << endl;
+			cout << stile[i][j] << endl;
 		}
 		
 	}
