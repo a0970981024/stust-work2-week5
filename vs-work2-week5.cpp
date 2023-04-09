0e9bf3387c12ecc28dec0b2729f89c01646e2553
日平 趙 <4b1g0034@office.stust.edu.tw>
2023-04-09 22:08:08 +0800

    撲克牌比大小 S黑桃 H愛心 D菱形 C梅花

--- vs-work2-week5.cpp
+++ vs-work2-week5.cpp
@@ -1,47 +1,99 @@
 ﻿#include <iomanip>
 #include <iostream>
 #include <string>
+#pragma warning(disable: 26446)
+#pragma warning(disable: 26482)
 using namespace std;
 
 int main() {
-	int x,y=1;
-	char temp[52] = {0},ctile[52][52],yy[52]={0};
-	
-	char y[] = {'a','1','b','1','c','1'};
-	string stile[52][52] ;
+	int x = 0, y = 1, temp[52] = { 0 }, itile[52][52][3], Itile[52] = { 0 }, temp2[52];
+	for (int i = 0; i < 51; i++) {
+		temp2[i] = i;
+		for (int j = 0; j < 51; j++) {
+			for (int t = 0; t < 3; t++) {
+				itile[i][j][t] = -1;
+			}
+		}
+	}
+	char ctile[52][52][3] = { 0 };
+	string stile[52][52], cpstile[52];
 	cout << "請輸入有幾疊牌:";
-	cin >> x;//x=2
-	
-	for (int i = 0 ; i < x; i++) {	//跑兩圈
-		cout << "請輸入第" << i+1 << "疊牌:";
-		for (int j = 0; j < 52; j++) {  
+	cin >> x;
+
+	for (int i = 0; i < x; i++) {
+		cout << "請輸入第" << i + 1 << "疊牌:";
+		for (int j = 0; j < 52; j++) {
 			cin >> stile[i][j];
 			if (cin.get() == '\n') {
 				j = 52;
-			}else{
+			}
+			else {
 				y++;
 			}
 		}
-		temp[x] = y;
-		y = 0;
+		temp[i] = y;
+		y = 1;
 	}
-
-	//if(stile[1][0]=='')
-	//cout << "Y=" << y << endl;
 	for (int i = 0; i < x; i++) {
-		for (int j = 0; j < temp[x]; j++) {
-			
-			
+		for (int j = 0; j < temp[i]; j++) {
+			for (int t = 0; t < 3; t++) {
+				ctile[i][j][t] = stile[i][j][t];
+				itile[i][j][t] = ctile[i][j][t];
+			}
 		}
 	}
-
-
-
-	for(int i = 0; i < x; i++) {
-		for (int j = 0; j < 52 ; j++) {
-			//cout << yy[j] << endl;
-			cout << stile[i][j] << endl;
+	for (int i = 0; i < x; i++) {
+		for (int j = 0; j < temp[i]; j++) {
+			switch (itile[i][j][0]) {
+			case 115:
+			case 83:	//s
+				Itile[j] = itile[i][j][0] + itile[i][j][1] + itile[i][j][2] + 300;
+				if (itile[i][j][2] > -1) Itile[j] += 8;
+				break;
+			case 104:
+			case 72://h
+				Itile[j] = itile[i][j][0] + itile[i][j][1] + itile[i][j][2] + 200;
+				if (itile[i][j][2] > -1) Itile[j] += 8;
+				break;
+			case 100:
+			case 68://d
+				Itile[j] = itile[i][j][0] + itile[i][j][1] + itile[i][j][2] + 100;
+				if (itile[i][j][2] > -1) Itile[j] += 8;
+				break;
+			case 99:
+			case 67://c
+				Itile[j] = itile[i][j][0] + itile[i][j][1] + itile[i][j][2];
+				if (itile[i][j][2] > -1) Itile[j] += 8;
+				break;
+			default:
+				break;
+			}
+			//cout << '\n' << Itile[j] << endl;
 		}
-		
+
+		cout << '\n';
+		cout << "第" << i + 1 << "疊牌: ";
+		for (int r = 0; r < temp[i]; r++) {
+			for (int t = 0; t < temp[i]; t++) {
+				if (Itile[t] < Itile[t + 1]) {
+					Itile[t] += Itile[t + 1];
+					Itile[t + 1] = Itile[t] - Itile[t + 1];
+					Itile[t] -= Itile[t + 1];
+
+					temp2[t] += temp2[t + 1];
+					temp2[t + 1] = temp2[t] - temp2[t + 1];
+					temp2[t] -= temp2[t + 1];
+				}
+			}
+		}
+		for (int n = 0; n < temp[i]; n++) {
+			cout << stile[i][temp2[n]] << ' ';
+		}
+		for (int q = 0; q < 52; q++) {
+
+			temp2[q] = q;
+		}
+		cout << '\n';
 	}
+
 }
